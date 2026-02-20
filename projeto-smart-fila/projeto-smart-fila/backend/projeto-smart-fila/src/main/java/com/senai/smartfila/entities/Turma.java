4cf8838d0ec53e6801764f2145083c4d632c7567
Braga <dev.victor.nascimento@gmail.com>
2026-02-20 15:52:16 -0300

    Add files via upload

---
+++ projeto-smart-fila/projeto-smart-fila/backend/projeto-smart-fila/src/main/java/com/senai/smartfila/entities/Turma.java
@@ -0,0 +1,60 @@
+package com.senai.smartfila.entities;
+
+
+
+import java.util.List;
+
+import com.fasterxml.jackson.annotation.JsonIgnore;
+
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.OneToMany;
+import jakarta.persistence.Table;
+import jakarta.validation.constraints.NotBlank;
+
+@Entity
+@Table(name = "tb_turmas")
+public class Turma {
+
+	@Id
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
+	private Long id;
+	
+	@NotBlank
+	@Column(name = "nome_turma")
+	private String nome;
+	
+	@OneToMany(mappedBy = "turma")
+	@JsonIgnore
+	private List<Aluno> alunos;
+	
+	//construtores
+	
+	public Turma() {
+		
+	}
+	
+	public Turma(String nome, List<Aluno> alunos ) {
+		this.alunos = alunos;
+		this.nome = nome;
+	}
+
+	public String getNome() {
+		return nome;
+	}
+
+	public void setNome(String nome) {
+		this.nome = nome;
+	}
+
+	public List<Aluno> getAlunos() {
+		return alunos;
+	}
+
+	public void setAlunos(List<Aluno> alunos) {
+		this.alunos = alunos;
+	}
+}
