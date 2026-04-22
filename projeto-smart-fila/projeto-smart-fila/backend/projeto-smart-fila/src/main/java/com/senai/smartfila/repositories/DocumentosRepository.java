f97a8ee58b03b7e00ecf11b3fff24c504e5adcdb
VgBraga <dev.victor.nascimento@gmail.com>
2026-04-22 12:52:12 -0300

    .

--- projeto-smart-fila/projeto-smart-fila/backend/projeto-smart-fila/src/main/java/com/senai/smartfila/repositories/DocumentosRepository.java
+++
@@ -1,9 +0,0 @@
-package com.senai.smartfila.repositories;
-
-import org.springframework.data.jpa.repository.JpaRepository;
-
-import com.senai.smartfila.entities.Documentos;
-
-public interface DocumentosRepository extends JpaRepository<Documentos, Long> {
-
-}
